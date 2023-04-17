4d766e702124dba77bd46b199b56499d95a72be9
chaerim <qkrdbsk28@naver.com>
2023-04-17 22:40:17 +0900

    project initial settings

---
+++ app.js
@@ -0,0 +1,25 @@
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+const { sequelize } = require("./models");
+const app = express();
+
+app.set("port", process.env.PORT || 8000);
+
+sequelize.sync({ force: false }).then(() => {
+  console.log("database connection success");
+});
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
+
+app.get("/", (req, res) => {
+  res.status(200).send("sns.md");
+});
+
+app.listen(app.get("port"), () => {
+  console.log(app.get("port"), "번 포트 대기 중");
+});
